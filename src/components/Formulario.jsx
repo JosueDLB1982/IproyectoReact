52cf4c1368ccf4e0e43c7d83a06d88bf5f1f0f58
Josue_Lopez <josuedlopez1982@gmail.com>
2021-12-23 17:39:54 -0300

    Formulario de ingreso de pacientes diseñado

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -9,8 +9,8 @@ const Formulario = () => {
                <span className="text-indigo-600 font-bold">Administralos</span>
            </p>
 
-           <form className="bg-white shadow-md rounded-lg px-5 py-10">
-               <div>
+           <form className="bg-white shadow-md rounded-lg px-5 py-10 mb-10">
+               <div className="mb-5">
                    <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
                        Nombre Mascota
                    </label>{" "}
@@ -22,6 +22,56 @@ const Formulario = () => {
                         className="border-2 w-full placeholder-gray-400 rounded-md"
                    />
                </div>
+               <div className="mb-5">
+                   <label htmlFor="propietario" className="block text-gray-700 uppercase font-bold">
+                       Nombre Propietario
+                   </label>{" "}
+
+                   <input
+                        id="propietario"
+                        type="text"
+                        placeholder="Nombre del Propietario"
+                        className="border-2 w-full placeholder-gray-400 rounded-md"
+                   />
+               </div>
+               <div className="mb-5">
+                   <label htmlFor="email" className="block text-gray-700 uppercase font-bold">
+                       email
+                   </label>{" "}
+
+                   <input
+                        id="email"
+                        type="email"
+                        placeholder="Dirección email para contactar al propietario"
+                        className="border-2 w-full placeholder-gray-400 rounded-md"
+                   />
+               </div>
+               <div className="mb-5">
+                   <label htmlFor="fechaAlta" className="block text-gray-700 uppercase font-bold">
+                       Alta
+                   </label>{" "}
+
+                   <input
+                        id="fechaAlta"
+                        type="date"
+                        className="border-2 w-full placeholder-gray-400 rounded-md"
+                   />
+               </div>
+               <div className="mb-5">
+                   <label htmlFor="sintomas" className="block text-gray-700 uppercase font-bold">
+                       Síntomas
+                   </label>
+                   <textarea
+                    id="sintomas"
+                    className="border-2 w-full placeholder-gray-400 rounded-md"
+                    placeholder="Describe los síntomas que presenta la mascota"
+                    />
+               </div>
+               <input
+                    type="submit"
+                    className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-800 cursor-pointer"
+                    value="Agregar Paciente"
+               />
            </form>
 
         </div>
