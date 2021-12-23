79baf2725a10fd332395f50dcae547d5d6fe9b95
Josue_Lopez <josuedlopez1982@gmail.com>
2021-12-23 13:48:03 -0300

    Componente ListadoPacientes y componente Formulario creados

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header" // Importamos el componente Header
+import Formulario from "./components/Formulario"
+import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() { 
   
   return (
     <>  
       <Header/> {/* Hacemos el llamdo al componente para que se muestre en pantalla. Se usa el estilo de etiquetas únicas como las img */}
+      <Formulario/>
+      <ListadoPacientes/>
     </> 
   )
 }
