ba1741ad1c048bc0c3c17a2c85285c84d9a0d4d1
Josue_Lopez <josuedlopez1982@gmail.com>
2021-12-23 16:36:10 -0300

    Refinado del componente Formulario

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,12 @@ function App() {
   return (
     <div className="container mx-auto mt-20">  
       <Header/> {/* Hacemos el llamdo al componente para que se muestre en pantalla. Se usa el estilo de etiquetas únicas como las img */}
-      <Formulario/>
-      <ListadoPacientes/>
+
+      <div className="mt-12 md:flex">
+        <Formulario/>
+        <ListadoPacientes/>
+      </div>
+      
     </div> 
   )
 }
