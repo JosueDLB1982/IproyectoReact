ff781a95e8bedb6aacefee0cd1fee8024b4da876
Josue_Lopez <josuedlopez1982@gmail.com>
2021-12-23 15:54:19 -0300

    Aplicacion de estilos al Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,10 @@
 function Header() {
 
     return(
-        <h1>Retornando desde Header</h1>
+        <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
+            Seguimiento Pacientes{' '} {/* Agrego un espacio vacio para separar las palabras */}
+            <span className="text-indigo-600">Veterinaria</span>
+        </h1>  
     )
 }
 
