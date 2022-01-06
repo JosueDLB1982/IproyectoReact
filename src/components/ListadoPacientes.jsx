0ab8015b25dbcadc93507030c0bc6ef0e603aa20
Josue_Lopez <josuedlopez1982@gmail.com>
2022-01-06 11:00:44 -0300

    Mostrar texto condicional en funcion de si hay o no pacientes en la lista

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,21 +1,33 @@
 import Paciente from "./Paciente"
 
-const ListadoPacientes = ({pacientes}) => {
-    console.log(pacientes)
+const ListadoPacientes = ({ pacientes }) => {
+    console.log(pacientes && pacientes.length) // cuenta la cantidad de elementos en el array, con esto podemos verificar si está vacío, para colocar texto condicional.
     return (
         <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-            <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
-            <p className="text-xl mt-5 mb-10 text-center">
-                Administra tus{' '}
-                <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
-            </p>
-                {pacientes.map(paciente => (
+            {pacientes && pacientes.length ? (
+                <>
+                    <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+                    <p className="text-xl mt-5 mb-10 text-center">
+                        Administra tus{' '}
+                        <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+                    </p>
+                    {pacientes.map(paciente => (
                         <Paciente
                             key={paciente.id}
                             paciente={paciente}
-                        /> 
-                    )
-                )}     
+                        />
+                    ))}
+                </>
+            ) :
+                <>
+                    <h2 className="font-black text-3xl text-center">Actualmente no tienes Pacientes</h2>
+                    <p className="text-xl mt-5 mb-10 text-center">
+                        Al agregarlos{' '}
+                        <span className="text-indigo-600 font-bold">Aparecerán Aquí</span>
+                    </p>
+                </>
+            }
+
         </div>
     )
 }
